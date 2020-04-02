51944d3a2d25f3cc62eba063011b25506d8743ef
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2020-04-02 15:31:18 +0530

    Add files via upload

---
+++ WPF3dExample3.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for WPF3dExample3.xaml
+    /// </summary>
+    public partial class WPF3dExample3 : Window
+    {
+        public WPF3dExample3()
+        {
+            InitializeComponent();
+           
+
+        }
+        private Model3DGroup CreateTriangleModel(Point3D p0, Point3D p1, Point3D p2)
+        {
+            MeshGeometry3D mymesh = new MeshGeometry3D();
+            mymesh.Positions.Add(p0);
+            mymesh.Positions.Add(p1);
+            mymesh.Positions.Add(p2);
+            mymesh.TriangleIndices.Add(0);
+            mymesh.TriangleIndices.Add(1);
+            mymesh.TriangleIndices.Add(2);
+            Vector3D Normal = CalculateTraingleNormal(p0, p1, p2);
+            mymesh.Normals.Add(Normal);
+            mymesh.Normals.Add(Normal);
+            mymesh.Normals.Add(Normal);
+            Material Material = new DiffuseMaterial(
+                  new SolidColorBrush(Colors.BlueViolet));
+            GeometryModel3D model = new GeometryModel3D(
+                mymesh, Material);
+            Model3DGroup Group = new Model3DGroup();
+            Group.Children.Add(model);
+            return Group;
+
+
+
+        }
+
+        private Vector3D CalculateTraingleNormal(Point3D p0, Point3D p1, Point3D p2)
+        {
+            Vector3D v0 = new Vector3D(
+                p1.X - p0.X, p1.Y - p0.Y, p1.Z - p0.Z);
+            Vector3D v1 = new Vector3D(
+                p2.X - p1.X, p2.Y - p1.Y, p2.Z - p1.Z);
+            return Vector3D.CrossProduct(v0, v1);
+        }
+
+        private void triangleButtonClick(object sender, RoutedEventArgs e)
+        {
+            Model3DGroup triangle = new Model3DGroup();
+            Point3D p0 = new Point3D(0, 0, 0);
+            Point3D p1 = new Point3D(5, 0, 0);
+            Point3D p2 = new Point3D(5, 0, 5);
+            Point3D p3 = new Point3D(0, 0, 5);
+            Point3D p4 = new Point3D(0, 5, 0);
+            Point3D p5 = new Point3D(5, 5, 0);
+            Point3D p6 = new Point3D(5, 5, 5);
+            triangle.Children.Add(CreateTriangleModel(p1, p4, p3));
+            triangle.Children.Add(CreateTriangleModel(p1, p4, p6));
+            triangle.Children.Add(CreateTriangleModel(p3, p1, p6));
+            ModelVisual3D Model = new ModelVisual3D();
+            Model.Content = triangle;
+            this.MainViewPort.Children.Add(Model);
+        }
+
+    }
+}
